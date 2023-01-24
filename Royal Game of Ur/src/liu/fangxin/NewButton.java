0e02cb859ab944a07c77406ef9fce0888bb99e30
342156965 <342156965@C-851VD22.tdsb.on.ca>
2023-01-24 10:55:45 -0500

    Nolan's driver

---
+++ Royal Game of Ur/src/liu/fangxin/NewButton.java
@@ -0,0 +1,28 @@
+package liu.fangxin;
+
+import javafx.scene.control.Button;
+
+public class NewButton extends Button {
+	private int row;
+	private int col;
+	
+	public NewButton (int ro, int co){
+		super();
+		row = ro;
+		col = co;
+	}
+	
+	public int getRow(){
+		return row;
+	}
+	public int getCol(){
+		return col;
+	}
+	
+	@Override
+	public String toString(){
+		return "[" + String.valueOf(getRow()) + "," + String.valueOf(getCol())+"]";
+	}
+
+
+}
